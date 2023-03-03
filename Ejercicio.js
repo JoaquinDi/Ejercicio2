672118a9bd82fbf823539cf4b6c2e5e9701f8e27
Joaquín Dionisio <103225171+JoaquinDi@users.noreply.github.com>
2023-03-03 14:35:47 -0300

    primer commit

---
+++ Ejercicio.js
@@ -0,0 +1,16 @@
+const fechaDeNacimiento = new Date(2001, 4, 3);
+const fechaLibro = new Date(1954, 6, 29);
+
+
+const misDatos = {
+    nombre: 'Joaquin Dionisio',
+    edad: 21,
+    desarrollador: true,
+    fechaNacimiento: fechaDeNacimiento,
+    libroFavorito: {
+        nombre: 'El señor de los anillos',
+        autor: 'J. R. R. Tolkien',
+        fecha: fechaLibro,
+        url: 'https://es.wikipedia.org/wiki/El_Señor_de_los_Anillos'
+    }
+}
